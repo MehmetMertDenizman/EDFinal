94fddca086df64716f00aea31aaa749c8d740a17
MehmetMertDenizman <amorylorch@gmail.com>
2022-09-12 22:26:41 +0300

    8 giriş

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+
+ProductManager productManager = new ProductManager(new EfProductDal());
+foreach (var item in productManager.GetAll())
+{
+    Console.WriteLine(item.ProductName);
+}
